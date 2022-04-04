9a7180a97c6d983245dc810011fc850d89d45738
Ricardo Paes <ricardo@likesistemas.com.br>
2022-04-03 23:42:34 -0300

    🩹 Fixing phpcsfixer errors

--- src/Config.php
+++ src/Config.php
@@ -7,33 +7,33 @@ use Illuminate\Support\Arr;
 use RuntimeException;
 
 class Config implements ArrayAccess {
-    
-    /**
-     * @var array
-     */
-    private $config = [];
+	
+	/**
+	 * @var array
+	 */
+	private $config = [];
 
-    public function __construct(array $config) {
-        $this->config = $config;
-    }
+	public function __construct(array $config) {
+		$this->config = $config;
+	}
 
-    public function get($key, $defaultVaue = null) {
-        return Arr::get($this->config, $key, $defaultVaue);
-    }
+	public function get($key, $defaultVaue = null) {
+		return Arr::get($this->config, $key, $defaultVaue);
+	}
 
-    public function offsetExists($offset) {
-        throw new RuntimeException('Not implemented because we didn\'t need it yet');
-    }
+	public function offsetExists($offset) {
+		throw new RuntimeException('Not implemented because we didn\'t need it yet');
+	}
 
-    public function offsetGet($offset) {
-        return $this->get($offset);
-    }
+	public function offsetGet($offset) {
+		return $this->get($offset);
+	}
 
-    public function offsetSet($offset, $value) {
-        throw new RuntimeException('Not implemented because we didn\'t need it yet');
-    }
+	public function offsetSet($offset, $value) {
+		throw new RuntimeException('Not implemented because we didn\'t need it yet');
+	}
 
-    public function offsetUnset($offset) {
-        throw new RuntimeException('Not implemented because we didn\'t need it yet');
-    }
+	public function offsetUnset($offset) {
+		throw new RuntimeException('Not implemented because we didn\'t need it yet');
+	}
 }
