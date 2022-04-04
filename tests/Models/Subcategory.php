321605a2e9b7b67a6d39fbc98aecb498f8ad6433
Ricardo Paes <ricardo@likesistemas.com.br>
2022-04-03 23:42:03 -0300

    🩹 Fixing phpstan errors

--- tests/Models/Subcategory.php
+++ tests/Models/Subcategory.php
@@ -9,9 +9,9 @@ use Illuminate\Database\Eloquent\Model;
  *
  * @property integer $id
  * @property string $name
+ *
  * @method static \Illuminate\Database\Eloquent\Builder|\Like\Eloquent\IdeHelper\Tests\Models\Subcategory whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\Like\Eloquent\IdeHelper\Tests\Models\Subcategory whereName($value)
- * @mixin \Eloquent
  */
 class Subcategory extends Model {
 	protected $fillable = [];
