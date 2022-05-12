a02c176a3f8045cdc4bf557f5b8598753aa10291
Ricardo Araújo Paes <38794747+ricardoapaes@users.noreply.github.com>
2022-05-12 16:49:07 -0300

    :bug: Fixing bugs in PHP 5.6 (#2)
    * :bug: Fixing bugs in PHP 5.6
    * 👷 Adding binary check
    * 🔨 Updating model comments
    * 💚 Fixing `php-stan` errors
    * 🩹 Configuring crlf binary files

--- tests/Models/Subcategory.php
+++ tests/Models/Subcategory.php
@@ -10,8 +10,11 @@ use Illuminate\Database\Eloquent\Model;
  * @property integer $id
  * @property string $name
  *
- * @method static \Illuminate\Database\Eloquent\Builder|\Like\Eloquent\IdeHelper\Tests\Models\Subcategory whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\Like\Eloquent\IdeHelper\Tests\Models\Subcategory whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Subcategory newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Subcategory newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Subcategory query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Subcategory whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Subcategory whereName($value)
  */
 class Subcategory extends Model {
 	protected $fillable = [];
